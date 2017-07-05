4e2499561bea73ad599478fd0bfc4af7f1563960
Arausi Daniel <arausid@yahoo.com>
2017-07-05 16:08:11 +0100

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 let app = express();
 
-app.use(express.static(path.join(__dirname,'..','ReactFnd','public')));
+app.use(express.static(path.join(__dirname, '..', 'ReactFnd', 'public')));
 
 let port = process.env.PORT || 9000;
 
+//express is highly knitted with the httpcreateServer thing
+//
+let server = http.createServer(app);
+let io = socketIO(server);
+
+//.on is used to listen for events
+//sockets in this context are the number of //users connected
+
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.on('disconnect', (socket) => {
+		console.log('User disconnected');
+	})
+})
 
 
-app.listen(port,()=>{
+
+server.listen(port, () => {
 	console.log(`app is listening on port ${port}`);
 });
-
-
